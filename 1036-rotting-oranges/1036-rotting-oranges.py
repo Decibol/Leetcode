a3f7b44402847ed816dacb600680e01e767aeb8e
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-07-06 22:27:00 +0530

    Time: 0 ms (100%), Space: 17.8 MB (44.3%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,32 +1,35 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         minutes = 0
-        queue = deque()
+        fresh_count = 0
+        q = deque()
         rows = len(grid)
         cols = len(grid[0])
-        fresh_count = 0
         
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == 2:
-                    queue.append([row, col])
-                if grid[row][col] == 1:
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    q.append([r, c])
+                if grid[r][c] == 1:
                     fresh_count += 1
         
-        directions = [[-1,0], [1, 0], [0, -1], [0, 1]]
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
-        while queue and fresh_count > 0:
-            for i in range(len(queue)):
-                r, c = queue.popleft()
-
+        while q and fresh_count > 0:
+            
+            for i in range(len(q)):
+                r, c = q.popleft()
+                
                 for dr, dc in directions:
-                    row, col = dr + r, dc + c
+                    row = dr + r
+                    col = dc + c
 
-                    if row < 0 or row == len(grid) or col < 0 or col == len(grid[0]) or grid[row][col] != 1:
+                    if row < 0 or row == rows or col < 0 or col == cols or grid[row][col] != 1:
                         continue
+                    
                     grid[row][col] = 2
-                    queue.append([row, col])
-                    fresh_count -= 1          
+                    q.append([row, col])
+                    fresh_count -= 1
             minutes += 1
         
         return minutes if fresh_count == 0 else -1
