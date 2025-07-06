5bc52b8de39023ea9989569bf0e924a9b75f8c4f
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-07-07 02:32:33 +0530

    Time: 0 ms (100%), Space: 16.5 MB (97.31%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int minutes = 0;
-        int fresh_count = 0;
-        queue<pair<int, int>> q;
         int rows = grid.size();
         int cols = grid[0].size();
+        int fresh_count = 0;
+        queue<pair<int, int>> q;
 
         for (int r = 0; r < rows; ++r){
             for (int c = 0; c < cols; ++c){
@@ -13,14 +13,14 @@ public:
                     q.push({r, c});
                 }
                 if (grid[r][c] == 1){
-                    fresh_count++;
+                    fresh_count += 1;
                 }
             }
         }
 
         vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         
-        while (!q.empty() && fresh_count > 0){
+        while(!q.empty() && fresh_count > 0){
             int size = q.size();
             for (int i = 0; i < size; ++i){
                 auto [r, c] = q.front();
@@ -33,12 +33,12 @@ public:
                     if (row < 0 || row == rows || col < 0 || col == cols || grid[row][col] != 1){
                         continue;
                     }
-                    q.push({row, col}); 
                     grid[row][col] = 2;
+                    q.push({row, col});
                     fresh_count -= 1;
                 }
             }
-            minutes += 1;
+            minutes++;
         }
         return (fresh_count == 0) ? minutes : -1;
     }
