cd598c8919502a5a5edebac1753a929f2e2dc6fe
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-07-21 18:28:48 +0530

    Time: 9 ms (34.39%), Space: 13.4 MB (36.47%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> roman_to_int = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int answer = 0;
+
+        for (int i = 0; i < s.size(); ++i){
+            if (i + 1 < s.size() && roman_to_int[s[i]] < roman_to_int[s[i + 1]]){
+                answer -= roman_to_int[s[i]];
+            }
+            else{
+                answer += roman_to_int[s[i]];
+            }
+        }
+
+        return answer;
+    }
+};
