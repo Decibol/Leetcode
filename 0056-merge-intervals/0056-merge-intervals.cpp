a43a491e0931ad7af897454a54558f8079d2db2a
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-07-21 19:03:08 +0530

    Time: 7 ms (41.76%), Space: 23.8 MB (80.14%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,14 +5,12 @@ public:
         sort(intervals.begin(), intervals.end());
         merged.push_back(intervals[0]);
 
-
         for (int i = 1; i < intervals.size(); ++i){
             vector<int>& last = merged.back();
 
             if (last[1] >= intervals[i][0]){
                 last[1] = max(last[1], intervals[i][1]);
             }
-
             else{
                 merged.push_back(intervals[i]);
             }
