ab063a3dad99969956dfb7cff0de62a32cbea7a8
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-07-21 19:00:40 +0530

    Time: 0 ms (100%), Space: 12.2 MB (70.71%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -3,20 +3,19 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int p1 = m - 1;
         int p2 = n - 1;
-
         int right = m + n - 1;
 
         while (p2 >= 0){
             if (p1 >= 0 && nums1[p1] > nums2[p2]){
                 nums1[right] = nums1[p1];
-                right--;
                 p1--;
             }
             else{
                 nums1[right] = nums2[p2];
-                right--;
                 p2--;
             }
+            
+            right--;
         }
     }
 };
