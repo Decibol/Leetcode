b0c924e53ffb5295528e6994ea775c1060bcd9d2
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-07-23 00:11:55 +0530

    Time: 0 ms (100%), Space: 8.6 MB (42.28%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -5,12 +5,13 @@ public:
         return n > 0 ? res : 1 / res;
     }
 
-    double helper(double x, long n){;
+    double helper(double x, long n){
         if (n == 0 || x == 1) return 1;
         if (x == 0) return 0;
 
         double res = helper(x, floor(n / 2));
         res = res*res;
-        return n % 2 == 0 ? res : res*x;
+
+        return n % 2 == 0 ? res : res * x;
     }
 };
