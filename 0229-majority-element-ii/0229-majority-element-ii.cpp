6e818cabd51b8df3dcf356e296edf368b9182442
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-07-23 17:41:16 +0530

    Time: 0 ms (100%), Space: 23.6 MB (79.06%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -4,8 +4,9 @@ public:
         int candidate1 = 0, candidate2 = 1;
         int count1 = 0, count2 = 0;
         int n = nums.size();
-
+        
         for (int i = 0; i < n; ++i){
+
             if (nums[i] == candidate1){
                 count1++;
             }
@@ -26,13 +27,14 @@ public:
             }
         }
 
-        vector<int> ans;
         count1 = 0, count2 = 0;
 
         for (int num : nums){
             if (num == candidate1) count1++;
             if (num == candidate2) count2++;
         }
+
+        vector<int> ans;
 
         if (count1 > n / 3) ans.push_back(candidate1);
         if (count2 > n / 3) ans.push_back(candidate2);
