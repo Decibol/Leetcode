ca376a8c5e493fbf66053a65a2c45d5a5524b120
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-07-25 17:47:05 +0530

    Time: 0 ms (100%), Space: 14.9 MB (20.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> pair;
-        
+
         for (int i = 0; i < nums.size(); ++i){
             int complement = target - nums[i];
+
             if (pair.find(complement) != pair.end()){
-                return {i, pair[complement]};
+                return {pair[complement], i};
             }
+
             pair[nums[i]] = i;
         }
+
         return {};
     }
 };
