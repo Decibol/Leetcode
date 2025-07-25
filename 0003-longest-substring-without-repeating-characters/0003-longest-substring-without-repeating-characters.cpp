72001d10fcf16cf78529a3bc0dd711235adccacc
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-07-25 22:46:48 +0530

    Time: 24 ms (15.63%), Space: 14.2 MB (31.05%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        unordered_set<char> charSet;
         int left = 0;
         int max_length = 0;
-        unordered_set<char> charSet;
-
-        for(int right = 0; right < s.size(); ++right){
+        
+        for (int right = 0; right < s.size(); ++right){
             while (charSet.find(s[right]) != charSet.end()){
                 charSet.erase(s[left]);
-                left += 1;
+                left++;
             }
+
             charSet.insert(s[right]);
             max_length = max(max_length, right - left + 1);
         }
+
         return max_length;
     }
 };
