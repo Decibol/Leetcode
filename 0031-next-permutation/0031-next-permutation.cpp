6f869a8cbffa5aa343eea855b6b1e0ebb4529e5d
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-07-28 11:16:05 +0530

    Time: 0 ms (100%), Space: 15.7 MB (67.52%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,19 +3,20 @@ public:
     void nextPermutation(vector<int>& nums) {
         int i = nums.size() - 2;
 
-        while(i >= 0 && nums[i] >= nums[i + 1]){
+        while (i >= 0 && nums[i] >= nums[i + 1]){
             i--;
         }
 
         if (i >= 0){
             int j = nums.size() - 1;
-            
-            while(nums[j] <= nums[i]){
+
+            while (nums[j] <= nums[i]){
                 j--;
             }
-            
+
             swap(nums[i], nums[j]);
         }
+
         reverse(nums.begin() + i + 1, nums.end());
     }
 };
