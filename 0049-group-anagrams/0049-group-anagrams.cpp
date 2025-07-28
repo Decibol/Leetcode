5d1c810b465441221793daa4a2594e2751069ed1
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-07-29 04:32:29 +0530

    Time: 12 ms (85.01%), Space: 24.9 MB (85.25%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> answer;
+        unordered_map<string, vector<string>> anagramGroups;
+
+        for (string s : strs){
+            string key = s;
+            sort(key.begin(), key.end());
+            anagramGroups[key].push_back(s);
+        }
+
+        for (const auto& pair : anagramGroups){
+            answer.push_back(pair.second);
+        }
+
+        return answer;
+    }
+};
