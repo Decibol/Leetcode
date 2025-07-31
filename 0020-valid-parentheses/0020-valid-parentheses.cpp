010432f0d9e51cee78aee274ce00ac7bf1fb7665
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-07-31 20:40:27 +0530

    Time: 0 ms (100%), Space: 9 MB (15.01%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<int> st;
         unordered_map<char, char> charSet = {
             {')', '('},
             {']', '['},
             {'}', '{'}
         };
 
+        stack<char> st;
+
         for (int i = 0; i < s.size(); ++i){
             if (charSet.find(s[i]) != charSet.end()){
                 if (st.empty() || st.top() != charSet[s[i]]){
                     return false;
                 }
-
+                
                 st.pop();
             }
             else{
