d87ea39c33a8b30d550f23d9634f2a2516383c96
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-03 18:42:44 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        if (s.size() != t.size()) return false;
 
-        return s == t ? true : false;
+        unordered_map<char, int> freq;
+
+        for (char c : s){
+            freq[c] += 1;
+        }
+
+        for (char c : t){
+            freq[c] -= 1;
+        }
+
+        bool allZero = all_of(freq.begin(), freq.end(), [](auto& pair) {
+            return pair.second == 0;
+        });
+
+        return allZero;
     }
 };
