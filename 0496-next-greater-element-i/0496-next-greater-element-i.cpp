4bb5424f582740adbd4bbb7de3cacaa505436b0b
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-07 22:38:11 +0530

    Time: 3 ms (42.31%), Space: 12.7 MB (74.37%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> pair;
         stack<int> st;
+        unordered_map<int, int> pair;
         vector<int> result;
-        result.reserve(nums1.size());
 
         for (int num : nums2){
             while (!st.empty() && num > st.top()){
                 pair[st.top()] = num;
                 st.pop();
             }
-
+            
             st.push(num);
         }
 
