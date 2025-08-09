02f1349747b9b1cea1b73bd60a4a6451f7317599
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-09 16:53:47 +0530

    Time: 0 ms (100%), Space: 26 MB (60.18%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,10 +4,10 @@ public:
         int n = height.size();
         if (n <= 2) return 0;
 
-        int maxLeft = height[0];
-        int maxRight = height[n - 1];
         int left = 1;
         int right = n - 2;
+        int maxLeft = height[0];
+        int maxRight = height[n - 1];
         int waterCount = 0;
 
         while (left <= right){
@@ -21,7 +21,6 @@ public:
 
                 left++;
             }
-
             else{
                 if (height[right] < maxRight){
                     waterCount += maxRight - height[right];
