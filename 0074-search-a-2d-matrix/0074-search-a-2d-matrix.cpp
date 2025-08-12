10a426fbd7378c492265d6dea9930a9d3c3c2937
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-12 20:26:45 +0530

    Time: 0 ms (100%), Space: 13.4 MB (42.56%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int ROWS = matrix.size();
-        int COLS = matrix[0].size();
+        int rows = matrix.size();
+        int cols = matrix[0].size();
 
-        int top = 0, bottom = ROWS - 1;
+        int top = 0, bottom = rows - 1;
         int row;
-
-        while(top <= bottom){
+        while (top <= bottom){
             row = (top + bottom) / 2;
 
             if (target < matrix[row][0]){
                 bottom = row - 1;
             }
-            else if (target > matrix[row][COLS - 1]){
+            else if (target > matrix[row][cols - 1]){
                 top = row + 1;
             }
             else{
@@ -21,17 +20,17 @@ public:
             }
         }
 
-        int left = 0, right = COLS - 1;
-        int mid;
+        int left = 0, right = cols - 1;
+        int col;
 
         while (left <= right){
-            mid = (left + right) / 2;
+            col = (left + right) / 2;
 
-            if (target < matrix[row][mid]){
-                right = mid - 1;
+            if (target < matrix[row][col]){
+                right = col - 1;
             }
-            else if (target > matrix[row][mid]){
-                left = mid + 1;
+            else if (target > matrix[row][col]){
+                left = col + 1;
             }
             else{
                 return true;
