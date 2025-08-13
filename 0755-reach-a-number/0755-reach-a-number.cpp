ddb7afefbdc3ddac7a2955b1dbfdde5ca7664b56
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-14 02:29:30 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.25%) - LeetHub

---
+++ 0755-reach-a-number/0755-reach-a-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int reachNumber(int target) {
+        target = abs(target);
+        
+        int step = 0;
+        int sum = 0;
+        
+        while (true){
+            step++;
+            sum += step;
+
+            if (sum == target){
+                return step;
+            }
+            else if (sum > target && (sum - target) % 2 == 0){
+                return step;
+            }
+        }
+    }
+};
