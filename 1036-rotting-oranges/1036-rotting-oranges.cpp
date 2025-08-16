4e2738c5b9ae1f568914fbdbef0431a488328da7
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-16 21:06:03 +0530

    Time: 0 ms (100%), Space: 16.7 MB (75.2%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int minutes = 0;
+        int fresh_count = 0;
         int rows = grid.size();
         int cols = grid[0].size();
-        int fresh_count = 0;
         queue<pair<int, int>> q;
 
-        for (int r = 0; r < rows; ++r){
-            for (int c = 0; c < cols; ++c){
-                if (grid[r][c] == 2){
-                    q.push({r, c});
+        for (int i = 0; i < rows; ++i){
+            for (int j = 0; j < cols; ++j){
+                if (grid[i][j] == 2){
+                    q.push({i, j});
                 }
-                if (grid[r][c] == 1){
-                    fresh_count += 1;
+                else if (grid[i][j] == 1){
+                    fresh_count++;
                 }
             }
         }
 
         vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        
-        while(!q.empty() && fresh_count > 0){
+
+        while (!q.empty() && fresh_count > 0){
             int size = q.size();
+
             for (int i = 0; i < size; ++i){
                 auto [r, c] = q.front();
                 q.pop();
@@ -30,16 +31,17 @@ public:
                     int row = r + dr;
                     int col = c + dc;
 
-                    if (row < 0 || row == rows || col < 0 || col == cols || grid[row][col] != 1){
-                        continue;
-                    }
+                    if (row < 0 || row == rows || col < 0 || col == cols || grid[row][col] != 1) continue;
+
                     grid[row][col] = 2;
                     q.push({row, col});
-                    fresh_count -= 1;
+                    fresh_count--;
                 }
             }
+
             minutes++;
         }
-        return (fresh_count == 0) ? minutes : -1;
+
+        return fresh_count != 0 ? -1 : minutes;
     }
 };
