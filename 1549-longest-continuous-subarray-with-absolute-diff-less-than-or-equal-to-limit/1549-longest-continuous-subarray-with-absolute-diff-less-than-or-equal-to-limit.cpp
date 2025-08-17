bb272d6d107964759f9008e4c6013708fb2073fc
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-18 02:10:06 +0530

    Time: 17 ms (85.24%), Space: 77.6 MB (70.82%) - LeetHub

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        deque<int> max, min;
+        deque<int> min, max;
         int left = 0;
         int right;
 
         for (right = 0; right < nums.size(); ++right){
-            while (!max.empty() && max.back() < nums[right]) max.pop_back();
-            while (!min.empty() && min.back() > nums[right]) min.pop_back();
+            while (!min.empty() && nums[right] < min.back()) min.pop_back();
+            while (!max.empty() && nums[right] > max.back()) max.pop_back();
 
-            max.push_back(nums[right]);
             min.push_back(nums[right]);
+            max.push_back(nums[right]);
 
             if (max.front() - min.front() > limit){
                 if (max.front() == nums[left]) max.pop_front();
                 if (min.front() == nums[left]) min.pop_front();
-
                 ++left;
             }
         }
 
-        return right - left;
+        return right - left; 
     }
 };
