46664efa3796efc96efe1ed8447e32e82d684088
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-19 17:53:21 +0530

    Time: 3 ms (27.87%), Space: 10.4 MB (97.85%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -3,7 +3,6 @@ public:
     void backtrack(int start, vector<int>& nums, vector<vector<int>>& result){
         if (start == nums.size()){
             result.push_back(nums);
-            return;
         }
 
         for (int i = start; i < nums.size(); ++i){
@@ -12,10 +11,10 @@ public:
             swap(nums[start], nums[i]);
         }
     }
-
+    
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> result;
-
+        
         backtrack(0, nums, result);
 
         return result;
