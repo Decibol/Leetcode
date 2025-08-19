2e2a33e4140f66b7564d1a0b92efbc275bc785b4
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-20 01:13:19 +0530

    Time: 37 ms (67.43%), Space: 62.4 MB (81.75%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    void backtrack(int start, int k, vector<int>& nums, vector<int>& current, vector<vector<int>>& result){
+    void backtrack(int start, int k, int n, vector<int>& current, vector<vector<int>>& result){
         if (current.size() == k){
             result.push_back(current);
             return;
         }
 
-        for (int i = start; i < nums.size(); ++i){
-            current.push_back(nums[i]);
-            backtrack(i + 1, k, nums, current, result);
+        for (int i = start; i <= n; ++i){
+            current.push_back(i);
+            backtrack(i + 1, k, n, current, result);
             current.pop_back();
         }
     }
-    
+
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> result;
         vector<int> current;
-        vector<int> nums(n);
-        iota(nums.begin(), nums.end(), 1); 
 
-        backtrack(0, k, nums, current, result);
+        backtrack(1, k, n, current, result);
 
         return result;
     }
