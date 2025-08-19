e89be66e9818dc6324d8cbb01e9af81375d085c7
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-20 02:00:10 +0530

    Time: 0 ms (100%), Space: 12.6 MB (86.2%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def applyOperations(self, nums):
+        n = len(nums)
+        for i in range(n - 1):
+            if nums[i] == nums[i + 1]:
+                nums[i] *= 2
+                nums[i + 1] = 0
+        
+        insert_pos = 0
+        for num in nums:
+            if num != 0:
+                nums[insert_pos] = num
+                insert_pos += 1
+        
+        for i in range(insert_pos, n):
+            nums[i] = 0
+        
+        return nums
