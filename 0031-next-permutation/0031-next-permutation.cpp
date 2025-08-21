28da41549a58a49761d80daa15b55c90e00b4846
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-22 00:36:09 +0530

    Time: 0 ms (100%), Space: 15.6 MB (92.33%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,15 +4,12 @@ public:
         int n = nums.size();
         int i = n - 2;
 
-        while (i >= 0 && nums[i] >= nums[i + 1]){
-            i--;
-        }
-
+        while (i >= 0 && nums[i] >= nums[i + 1]) --i;
         if (i >= 0){
             int j = n - 1;
-            
+
             while (nums[j] <= nums[i]){
-                j--;
+                --j;
             }
 
             swap(nums[i], nums[j]);
