df166fe4ff5c0e6413b20e028f4dcc54dd8672ce
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-22 00:10:49 +0530

    Time: 0 ms (100%), Space: 10.2 MB (74.67%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void backtrack(int row, int n, vector<string>& board, vector<bool>& cols, vector<bool>& posDiagonal, vector<bool>& negDiagonal, vector<vector<string>>& result){
+        if (row == n){
+            result.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; ++col){
+            if (cols[col] || posDiagonal[row + col] || negDiagonal[row - col + n - 1]) continue;
+
+            cols[col] = true;
+            posDiagonal[row + col] = true;
+            negDiagonal[row - col + n - 1] = true;
+            board[row][col] = 'Q';
+
+            backtrack(row + 1, n, board, cols, posDiagonal, negDiagonal, result);      
+
+            cols[col] = false;
+            posDiagonal[row + col] = false;
+            negDiagonal[row - col + n - 1] = false;
+            board[row][col] = '.';
+
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        int row = 0;
+        vector<string> board(n, string(n, '.'));
+        vector<bool> cols(n, false);
+        vector<bool> posDiagonal(2 * n - 1, false);
+        vector<bool> negDiagonal(2 * n - 1, false);
+        vector<vector<string>> result;
+
+        backtrack(0, n, board, cols, posDiagonal, negDiagonal, result);
+
+        return result;
+    }
+};
