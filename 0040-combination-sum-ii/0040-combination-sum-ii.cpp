839b5601ae97fad86668043aa668461bfa78dc59
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-22 18:11:48 +0530

    Time: 5 ms (16%), Space: 14 MB (83.08%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    void backtrack(int start, int target, vector<int>& candidates, int currSum, vector<int>& current, vector<vector<int>>& result){
+    void backtrack(int start, int target, int currSum, vector<int>& candidates, vector<int>& current, vector<vector<int>>& result){
         if (currSum == target){
             result.push_back(current);
             return;
         }
 
         for (int i = start; i < candidates.size(); ++i){
-            if (i > start && candidates[i] == candidates[i - 1]) continue;
             if (currSum > target) break;
+            if (i > start && candidates[i] == candidates[i - 1]) continue;
 
             current.push_back(candidates[i]);
-            backtrack(i + 1, target, candidates, currSum + candidates[i], current, result);
+            backtrack(i + 1, target, currSum + candidates[i], candidates, current, result);
             current.pop_back();
         }
     }
@@ -19,11 +19,11 @@ public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<int> current;
         vector<vector<int>> result;
-        sort(candidates.begin(), candidates.end());
-
         int currSum = 0;
 
-        backtrack(0, target, candidates, currSum, current, result);
+        sort(candidates.begin(), candidates.end());
+
+        backtrack(0, target, currSum, candidates, current, result);
 
         return result;
     }
