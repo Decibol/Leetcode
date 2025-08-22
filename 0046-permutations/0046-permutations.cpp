0cad71dc4ccd2305a4d9bf81818c0a353a1a3251
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-22 18:18:17 +0530

    Time: 2 ms (36.78%), Space: 10.5 MB (86.85%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void backtrack(vector<int>& nums, vector<bool>& used, vector<int>& current, vector<vector<int>>& result){
+    void backtrack(int start, vector<int>& nums, vector<bool>& used, vector<int>& current, vector<vector<int>>& result){
         if (current.size() == nums.size()){
             result.push_back(current);
             return;
@@ -11,18 +11,18 @@ public:
 
             used[i] = true;
             current.push_back(nums[i]);
-            backtrack(nums, used, current, result);
+            backtrack(start + 1, nums, used, current, result);
             current.pop_back();
             used[i] = false;
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> result;
         vector<int> current;
+        vector<vector<int>> result;
         vector<bool> used(nums.size(), false);
-        
-        backtrack(nums, used, current, result);
+
+        backtrack(0, nums, used, current, result);
 
         return result;
     }
