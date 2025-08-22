f908691fde1f0d7156760dc2eed2e23f75bf2ce9
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-22 22:30:49 +0530

    Time: 2 ms (93.21%), Space: 23.9 MB (31.23%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> merged;
-        merged.push_back(intervals[0]);
         sort(intervals.begin(), intervals.end());
-
+        merged.push_back(intervals[0]);
 
         for (int i = 1; i < intervals.size(); ++i){
             vector<int>& last = merged.back();
