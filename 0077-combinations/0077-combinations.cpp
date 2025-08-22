a059b5aee1afe528135778ba36ef78086368510c
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-22 17:57:49 +0530

    Time: 35 ms (89.51%), Space: 62.5 MB (81.75%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -8,14 +8,15 @@ public:
 
         for (int i = start; i <= n; ++i){
             current.push_back(i);
+
             backtrack(i + 1, k, n, current, result);
+
             current.pop_back();
         }
     }
-
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> result;
         vector<int> current;
+        vector<vector<int>> result;
 
         backtrack(1, k, n, current, result);
 
