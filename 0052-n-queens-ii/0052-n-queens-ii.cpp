e28408a2be0d144de3bbd9c1d32de57861c8ab5d
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-24 00:39:51 +0530

    Time: 0 ms (100%), Space: 7.8 MB (90.52%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void backtrack(int row, int n, vector<bool>& cols, vector<bool>& posDiagonal, vector<bool>& negDiagonal, int& count){
+        if (row == n){
+            count++;
+            return;
+        }
+
+        for (int col = 0; col < n; ++col){
+            if (cols[col] || posDiagonal[row + col] || negDiagonal[row - col + n - 1]) continue;
+
+            cols[col] = true;
+            posDiagonal[row + col] = true;
+            negDiagonal[row - col + n - 1] = true;
+
+            backtrack(row + 1, n, cols, posDiagonal, negDiagonal, count);
+            
+            cols[col] = false;
+            posDiagonal[row + col] = false;
+            negDiagonal[row - col + n - 1] = false;
+        }
+    }
+
+    int totalNQueens(int n) {
+        vector<bool> cols(n, false);
+        vector<bool> posDiagonal(2*n - 1, false);
+        vector<bool> negDiagonal(2*n - 1, false);
+        int count = 0;
+
+        backtrack(0, n, cols, posDiagonal, negDiagonal, count);
+
+        return count;
+    }
+};
