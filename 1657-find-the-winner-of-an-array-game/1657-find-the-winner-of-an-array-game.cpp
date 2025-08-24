943f446e256a7499b412fe83eeded7efc4b3d8c1
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-08-24 16:27:17 +0530

    Time: 0 ms (100%), Space: 67.2 MB (54.42%) - LeetHub

---
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k) {
+        int current = arr[0];
+        int win_count = 0;
+
+        for (int i = 1; i < arr.size(); ++i){
+            if (arr[i] > current){
+                current = arr[i];
+                win_count = 1;
+            }
+            else{
+                ++win_count;
+            }
+
+            if (win_count == k) break;
+        }
+
+        return current;
+    }
+};
