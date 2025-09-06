d6b76ec0686a40965f85821423947c489b7be3c5
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-09-06 23:30:14 +0530

    Time: 4 ms (9.22%), Space: 16 MB (54.76%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void bfs(TreeNode *root, vector<vector<int>>& result){
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()){
+            int size = q.size();
+            vector<int> current;
+
+            for (int i = 0; i < size; ++i){
+                TreeNode* node = q.front();
+                q.pop();
+
+                current.push_back(node->val);
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            result.push_back(current);
+        }
+    }
+
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        if (!root) return {};
+
+        vector<vector<int>> result;
+
+        bfs(root, result);
+
+        reverse(result.begin(), result.end());
+
+        return result;
+    }
+};
