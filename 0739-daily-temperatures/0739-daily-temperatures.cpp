6c692b310b79bd62df0e83e8916b6a2a7e621823
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-09-08 05:03:17 +0530

    Time: 31 ms (25.44%), Space: 107.2 MB (51.21%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        vector<int> answer(temperatures.size(), 0);
         stack<int> st;
+        vector<int> result(temperatures.size(), 0);
 
         for (int i = 0; i < temperatures.size(); ++i){
             while (!st.empty() && temperatures[i] > temperatures[st.top()]){
-                answer[st.top()] = i - st.top();
+                result[st.top()] = i - st.top();
                 st.pop();
             }
 
             st.push(i);
         }
 
-        return answer;
+        return result;
     }
 };
