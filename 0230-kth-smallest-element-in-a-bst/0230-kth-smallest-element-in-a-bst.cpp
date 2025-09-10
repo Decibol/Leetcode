fe8124cbbdf6b5da3af09f47a60cc2896a354993
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-09-10 17:37:06 +0530

    Time: 0 ms (100%), Space: 24.4 MB (42.29%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void dfs(TreeNode *root, int& k, int& count, int& result) {
+        if (!root || count >= k) return;
+
+        dfs(root->left, k, count, result);
+
+        if (count >= k) return;
+
+        ++count;
+        if (count == k) {
+            result = root->val;
+            return;
+        }
+
+        dfs(root->right, k, count, result);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        int result = 0;
+        int count = 0;
+
+        dfs(root, k, count, result);
+
+        return result;
+    }
+};
