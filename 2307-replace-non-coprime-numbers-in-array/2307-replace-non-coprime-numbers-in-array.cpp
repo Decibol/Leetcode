7fa462887a1c02884a208569191812ca9edad4ee
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-09-17 01:18:24 +0530

    Time: 16 ms (78.34%), Space: 125.1 MB (82.95%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    bool areCoprime(int num1, int num2) {
-        return gcd(num1, num2) == 1;
-    }
-
     vector<int> replaceNonCoprimes(vector<int>& nums) {
         vector<int> st;
 
