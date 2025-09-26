663977f263499357ae41ea931d08890eeb6e7954
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-09-27 02:19:36 +0530

    Time: 0 ms (100%), Space: 12.5 MB (5.77%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+private:
+    unordered_map<Node*, Node*> clones;
+
+public:
+    Node* dfs(Node* node){
+        vector<Node*> neighborNodes;
+        Node *copy = new Node(node->val);
+        clones[node] = copy;
+
+        for (auto neighbor : node->neighbors){
+            if (clones.find(neighbor) != clones.end()){
+                neighborNodes.push_back(clones[neighbor]);
+            }
+            else {
+                neighborNodes.push_back(dfs(neighbor));
+            }
+        }
+
+        copy->neighbors = neighborNodes;
+
+        return copy;
+    }
+
+    Node* cloneGraph(Node* node) {
+        if (node == nullptr) return nullptr;
+
+        return dfs(node);
+    }
+};
