ee96f063e36be9c7adb62235411b8a728197cf95
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-09-29 00:33:20 +0530

    Time: 0 ms (100%), Space: 16.9 MB (93.94%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,35 +11,35 @@
  */
 class Solution {
 public:
-    void bfs(TreeNode *root, vector<vector<int>>& result){
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()){
-            int size = q.size();
-            vector<int> current;
-            current.reserve(size);
-
-            for (int i = 0; i < size; ++i){
-                TreeNode *node = q.front();
-                q.pop();
-
-                current.push_back(node->val);
-
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-
-            result.push_back(current);
-        }
-    }
-
     vector<vector<int>> levelOrder(TreeNode* root) {
         if (!root) return {};
         vector<vector<int>> result;
 
+        auto bfs = [&](TreeNode *root, vector<vector<int>>& result){
+            queue<TreeNode*> q;
+            q.push(root);
+
+            while (!q.empty()){
+                int size = q.size();
+                vector<int> current;
+                current.reserve(size);
+
+                for (int i = 0; i < size; ++i){
+                    TreeNode *node = q.front();
+                    q.pop();
+
+                    current.push_back(node->val);
+
+                    if (node->left) q.push(node->left);
+                    if (node->right) q.push(node->right);
+                }
+
+                result.push_back(current);
+            }
+        };
+
         bfs(root, result);
 
-        return result;    
+        return result;
     }
 };
