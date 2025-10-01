af912494fad9395bc3e7982b11ce7e8d3d07bb1e
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-10-01 19:03:06 +0530

    Time: 33 ms (23.66%), Space: 23.6 MB (28.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void bfs(int row, int col, vector<vector<char>>& grid, vector<vector<bool>>& visited, vector<pair<int, int>>& directions){
+    void bfs(int row, int col, vector<vector<char>>& grid, vector<pair<int, int>>& directions, vector<vector<bool>>& visited){
         queue<pair<int, int>> q;
         q.push({row, col});
         visited[row][col] = true;
@@ -20,14 +20,13 @@ public:
                         q.push({nr, nc});
                     }
                 }
-            }            
+            }
         }
     }
 
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
-        int n = grid[0].size();
-
+        int n = grid[0].size();        
         vector<vector<bool>> visited(m, vector<bool>(n, false));
         vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
@@ -36,12 +35,12 @@ public:
         for (int i = 0; i < m; ++i){
             for (int j = 0; j < n; ++j){
                 if (grid[i][j] == '1' && !visited[i][j]){
-                    bfs(i, j, grid, visited, directions);
+                    bfs(i, j, grid, directions, visited);
                     ++count;
                 }
             }
         }
 
-        return count;
+        return count;     
     }
 };
