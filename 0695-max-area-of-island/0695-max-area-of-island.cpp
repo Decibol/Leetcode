ad2c309c49ed472a252ee23b65aaf410189be49b
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-10-01 19:39:01 +0530

    Time: 5 ms (32.77%), Space: 31.7 MB (20.15%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    void bfs(int& row, int& col, int& maxArea, vector<vector<int>>& grid, vector<pair<int, int>>& directions, vector<vector<bool>>& visited){
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        visited[row][col] = true;
+        int area = 1;
+
+        while (!q.empty()){
+            auto [r, c] = q.front();
+            q.pop();
+
+            for (auto const& [dr, dc] : directions){
+                int nr = r + dr;
+                int nc = c + dc;
+
+                if (nr >= 0 && nr < grid.size() && nc >= 0 && nc < grid[0].size()){
+                    if (grid[nr][nc] == 1 && !visited[nr][nc]){
+                        visited[nr][nc] = true;
+                        ++area;
+                        q.push({nr, nc});
+                    }
+                }
+            }
+
+            maxArea = max(maxArea, area);
+        }
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        int maxArea = 0;
+        
+        for (int i = 0; i < m; ++i){
+            for (int j = 0; j < n; ++j){
+                if (grid[i][j] == 1 && !visited[i][j]){
+                    bfs(i, j, maxArea, grid, directions, visited);
+                }
+            }
+        }
+
+        return maxArea;
+    }
+};
