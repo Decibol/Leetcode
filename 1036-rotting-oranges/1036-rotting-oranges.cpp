dc78bb658402b2db3f5d3ef78a81113c2ac497f9
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-10-02 21:12:15 +0530

    Time: 0 ms (100%), Space: 16.7 MB (74.96%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int minutes = 0;
-        int fresh_count = 0;
-        int rows = grid.size();
-        int cols = grid[0].size();
+        int fresh = 0;
+        int time = 0;
         queue<pair<int, int>> q;
 
-        for (int i = 0; i < rows; ++i){
-            for (int j = 0; j < cols; ++j){
-                if (grid[i][j] == 2){
-                    q.push({i, j});
-                }
-                else if (grid[i][j] == 1){
-                    fresh_count++;
-                }
+        for (int i = 0; i < grid.size(); ++i){
+            for (int j = 0; j < grid[0].size(); ++j){
+                if (grid[i][j] == 1) ++fresh;
+                if (grid[i][j] == 2) q.push({i, j});
             }
         }
 
         vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-        while (!q.empty() && fresh_count > 0){
+        while (!q.empty() && fresh > 0){
             int size = q.size();
 
             for (int i = 0; i < size; ++i){
@@ -28,20 +22,20 @@ public:
                 q.pop();
 
                 for (auto [dr, dc] : directions){
-                    int row = r + dr;
-                    int col = c + dc;
+                    int nr = r + dr;
+                    int nc = c + dc;
 
-                    if (row < 0 || row == rows || col < 0 || col == cols || grid[row][col] != 1) continue;
+                    if (nr < 0 || nr == grid.size() || nc < 0 || nc == grid[0].size() || grid[nr][nc] != 1) continue;
 
-                    grid[row][col] = 2;
-                    q.push({row, col});
-                    fresh_count--;
+                    grid[nr][nc] = 2;
+                    q.push({nr, nc});
+                    --fresh;
                 }
             }
 
-            minutes++;
+            ++time;
         }
 
-        return fresh_count != 0 ? -1 : minutes;
+        return fresh != 0 ? -1 : time;
     }
 };
