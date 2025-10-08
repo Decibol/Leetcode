7271b9de64a445f891236ab7b7f44f4872b322bb
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-10-09 00:40:07 +0530

    Time: 0 ms (100%), Space: 28.3 MB (60.68%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
-    void bfs(int& row, int& col, int& maxArea, vector<vector<int>>& grid, vector<pair<int, int>>& directions, vector<vector<bool>>& visited){
-        queue<pair<int, int>> q;
-        q.push({row, col});
-        visited[row][col] = true;
-        int area = 1;
-
-        while (!q.empty()){
-            auto [r, c] = q.front();
-            q.pop();
-
-            for (auto const& [dr, dc] : directions){
-                int nr = r + dr;
-                int nc = c + dc;
-
-                if (nr >= 0 && nr < grid.size() && nc >= 0 && nc < grid[0].size()){
-                    if (grid[nr][nc] == 1 && !visited[nr][nc]){
-                        visited[nr][nc] = true;
-                        ++area;
-                        q.push({nr, nc});
-                    }
-                }
-            }
-
-            maxArea = max(maxArea, area);
+    void dfs(int r, int c, vector<vector<int>>& grid, vector<vector<bool>>& visited, int& area){
+        if (r < 0 || r == grid.size() || c < 0 || c == grid[0].size() || visited[r][c] || grid[r][c] != 1){
+            return;
         }
+
+        ++area;
+        visited[r][c] = true;
+
+        dfs(r + 1, c, grid, visited, area);
+        dfs(r - 1, c, grid, visited, area);
+        dfs(r, c + 1, grid, visited, area);
+        dfs(r, c - 1, grid, visited, area);
     }
 
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<bool>> visited(m, vector<bool>(n, false));
-        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
         int maxArea = 0;
-        
+
         for (int i = 0; i < m; ++i){
             for (int j = 0; j < n; ++j){
                 if (grid[i][j] == 1 && !visited[i][j]){
-                    bfs(i, j, maxArea, grid, directions, visited);
+                    int area = 0;
+                    dfs(i, j, grid, visited, area);
+                    maxArea = max(maxArea, area);
                 }
             }
         }
