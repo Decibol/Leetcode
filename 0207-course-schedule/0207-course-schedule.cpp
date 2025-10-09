01625665e4f8dd37decafd08b15577ca192ee383
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-10-10 01:05:37 +0530

    Time: 14 ms (10.79%), Space: 22.9 MB (5.18%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    unordered_set<int> visited;
-
-    bool dfs(int course, unordered_map<int, vector<int>>& adj){
-        if (adj[course].empty()){
-            return true;
-        }
-
-        if (visited.count(course)){
-            return false;
-        }
-
-        visited.insert(course);
-
-        for (int p : adj[course]){
-            if (!dfs(p, adj)) return false;
-        }
-
-        adj[course].clear();
-
-        return true;
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map<int, vector<int>> adj;
+        unordered_set<int> visited;
 
-        for (auto const& pair : prerequisites){
+        for (auto pair : prerequisites){
             adj[pair[0]].push_back(pair[1]);
         }
 
+        function<bool(int)> dfs = [&](int course){
+            if (adj[course].empty()) return true;
+
+            if (visited.count(course)) return false;
+            visited.insert(course);
+            
+            for (int p : adj[course]){
+                if (!dfs(p)) return false;
+            }
+
+            adj[course].clear();
+
+            return true;
+        };
+
         for (int i = 0; i < numCourses; ++i){
-            if (!dfs(i, adj)) return false;
+            if (!dfs(i)) return false;
         }
 
         return true;
