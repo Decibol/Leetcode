619639f8e010814b1926c7ba478b2f97f715e05f
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-10-10 19:30:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> adj;
+        vector<vector<int>> adj(numCourses);
         unordered_set<int> visited;
 
-        for (auto pair : prerequisites){
+        for (auto const& pair : prerequisites){
             adj[pair[0]].push_back(pair[1]);
         }
 
@@ -13,12 +13,10 @@ public:
 
             if (visited.count(course)) return false;
             visited.insert(course);
-            
-            for (int p : adj[course]){
-                if (!dfs(p)) return false;
-            }
 
-            adj[course].clear();
+            for (int& x : adj[course]){
+                if (!dfs(x)) return false;
+            }
 
             return true;
         };
