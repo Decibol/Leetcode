8ebf23a3f72c25704339b98d3c0dc0134b3b68d3
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-10-27 20:09:04 +0530

    Time: 0 ms (100%), Space: 27.4 MB (54.97%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
@@ -1,41 +1,18 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        vector<int> devices(bank.size());
+        int prev = 0;
+        int beams = 0;
 
-        for (int i = 0; i < bank.size(); ++i){
-            int count = 0;
+        for (const string &row : bank){
+            int cnt = count(row.begin(), row.end(), '1');
 
-            for (int j = 0; j < bank[i].size(); ++j){
-                if (bank[i][j] == '1') ++count;
+            if (cnt > 0){
+                beams += prev * cnt;
+                prev = cnt;
             }
-
-            devices[i] = count;
         }
 
-        if (devices.size() == 1) return 0;
-
-        int numBeams = 0;
-        int i = 0;
-
-        while (i < devices.size() - 1 && devices[i] == 0) ++i;
-
-        while (i < devices.size() - 1){
-            int j = i + 1;
-
-            if (j == i + 1 && devices[j] != 0){
-                numBeams += devices[i] * devices[j];
-                i = j;
-                
-                continue;
-            }
-
-            while (j < devices.size() - 1 && devices[j] == 0) ++j;
-
-            numBeams += devices[i] * devices[j];
-            i = j;
-        }
-
-        return numBeams;
+        return beams;
     }
 };
