0ec433f146c6d3c7049723d410b2619784b8bc1b
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-10-30 01:39:22 +0530

    Time: 0 ms (100%), Space: 19.2 MB (97.47%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited){
+    void dfs(int node, vector<vector<int>>& isConnected, vector<bool>& visited){
         if (visited[node]) return;
-
+        
         visited[node] = true;
 
-        for (int connection : adj[node]){
-            dfs(connection, adj, visited);
+        for (int j = 0; j < isConnected.size(); ++j){
+            if (isConnected[node][j] == 1 && !visited[j]){
+                dfs(j, isConnected, visited);
+            }
         }
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<vector<int>> adj(n);
-
-        for (int i = 0; i < n; ++i){
-            for (int j = 0; j < n; ++j){
-                if (isConnected[i][j] == 1) adj[i].push_back(j);
-            }
-        }
-
-        vector<bool> visited(n, false); 
+        vector<bool> visited(n, false);
         int count = 0;
 
-        for (int i = 0; i < adj.size(); ++i){
-            if (!visited[i]){
-                dfs(i, adj, visited);
+        for (int i = 0; i < n; ++i){
+            if (!visited[i]) {
+                dfs(i, isConnected, visited);
                 ++count;
             }
         }
