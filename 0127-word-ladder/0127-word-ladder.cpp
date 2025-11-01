d63c6104bfb69ddfee6ae93bde4902c35920416a
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-11-02 02:54:24 +0530

    Time: 51 ms (80.44%), Space: 19.8 MB (89.11%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -2,35 +2,45 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set<string> wordSet(wordList.begin(), wordList.end());
+        wordSet.erase(beginWord);
+        
+        if (!wordSet.count(endWord)) return 0;
+        if (beginWord == endWord) return 1;
+
         queue<string> q;
         q.push(beginWord);
-        int count = 0;
+        int count = 1;
 
-        while (!q.empty() && wordSet.count(endWord)){
+        while (!q.empty()){
             int size = q.size();
 
             for (int i = 0; i < size; ++i){
                 string s = q.front();
                 q.pop();
                 string copy = s;
-                wordSet.erase(s);
 
-                for (char c = 'a'; c <= 'z'; ++c){
-                    for (int i = 0; i < copy.size(); ++i){
-                        copy[i] = c;
+                for (int j = 0; j < copy.size(); ++j){
+                    char original = copy[j];
+
+                    for (char c = 'a'; c <= 'z'; ++c){
+                        if (c == original) continue;
+                        copy[j] = c;
+
+                        if (copy == endWord) return count + 1;
 
                         if (wordSet.count(copy)){
                             q.push(copy);
+                            wordSet.erase(copy);
                         }
-                        
-                        copy = s;
                     }
+                    
+                    copy[j] = original;
                 }
             }
 
             ++count;
         }
 
-        return wordSet.count(endWord) ? 0 : count;
+        return 0;
     }
 };
