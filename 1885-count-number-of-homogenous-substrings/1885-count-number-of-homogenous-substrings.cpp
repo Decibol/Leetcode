6c30714578b5705dd298c498f61dc5e6ae3e085e
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-11-16 22:37:21 +0530

    Time: 4 ms (56.8%), Space: 14.9 MB (95.32%) - LeetHub

--- 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.cpp
+++ 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     int countHomogenous(string s) {
-        unordered_map<string, long long> freq;
-        long long result = 0;
-        long long mod = 1e9 + 7;
-        string homo = "";
+        int result = 0;
+        int n = 0;
+        int mod = 1e9 + 7;
 
         for (int i = 0; i < s.size(); ++i){
-            if (i == 0 || homo.back() == s[i]){
-                homo += s[i];
+            if (i == 0 || s[i] == s[i - 1]){
+                ++n;
             }
             else {
-                freq[homo]++;
-                homo = s[i];
+                n = 1;
             }
+
+            result = (result + n) % mod;
         }
 
-        freq[homo]++;
-
-        for (auto [str, fre] : freq){
-            long long n = str.size();
-            long long sum = fre * ((n * (n + 1)) / 2);
-            result += sum;
-        }
-
-        result %= mod;
-        
-        return (int)result;        
+        return result;
     }
 };
