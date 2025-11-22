00c6dbe50306a986d6ea6ef9d3b14d7acb4f3160
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-11-22 17:49:43 +0530

    Time: 67 ms (42.35%), Space: 60.4 MB (73.38%) - LeetHub

---
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    auto static cmp(string& a, string& b){
+        if (a.size() == b.size()){
+            return a < b;
+        }
+
+        return a.size() < b.size();
+    }
+
+    string kthLargestNumber(vector<string>& nums, int k) {
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end(), cmp);
+
+        return nums[n - k];    
+    }
+};
