a428d5ddab9649ac6a040c502ea96bc484287a24
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-12-03 20:14:20 +0530

    Time: 27 ms (56.87%), Space: 23.5 MB (26.41%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-    void dfs(int r, int c, vector<vector<char>>& grid, vector<vector<bool>>& visited){
-        if (r < 0 || r == grid.size() || c < 0 || c == grid[0].size() || visited[r][c] || grid[r][c] != '1'){
-            return;
+    void bfs(int& row, int& col, vector<vector<char>>& grid, vector<pair<int, int>>& directions, vector<vector<bool>>& visited){
+        queue<pair<int, int>> q;
+        q.push({row, col});
+
+        while (!q.empty()){
+            auto [r, c] = q.front();
+            q.pop();
+
+            for (auto const& [dr, dc] : directions){
+                int nr = r + dr;
+                int nc = c + dc;
+
+                if (nr < 0 || nr == grid.size() || nc < 0 || nc == grid[0].size() || grid[nr][nc] == '0' || visited[nr][nc]) continue;
+
+                visited[nr][nc] = true;
+                q.push({nr, nc});
+            }
         }
-
-        visited[r][c] = true;
-
-        dfs(r + 1, c, grid, visited);
-        dfs(r - 1, c, grid, visited);
-        dfs(r, c + 1, grid, visited);
-        dfs(r, c - 1, grid, visited);
     }
 
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<bool>> visited(m, vector<bool>(n, false));
+        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         int count = 0;
 
         for (int i = 0; i < m; ++i){
             for (int j = 0; j < n; ++j){
                 if (grid[i][j] == '1' && !visited[i][j]){
-                    dfs(i, j, grid, visited);
+                    bfs(i, j, grid, directions, visited);
                     ++count;
                 }
             }
