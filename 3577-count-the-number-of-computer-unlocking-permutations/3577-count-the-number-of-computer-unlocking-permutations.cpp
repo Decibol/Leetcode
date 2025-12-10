94e09b5dc8459863292be1e653876ce5794f73c9
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-12-10 18:00:05 +0530

    Time: 0 ms (100%), Space: 93 MB (23.5%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
@@ -3,17 +3,14 @@ public:
     int countPermutations(vector<int>& complexity) {
         int MOD = 1e9 + 7;
         int comp = complexity[0];
+        long long ans = 1;
 
         for (int i = 1; i < complexity.size(); ++i){
             if (complexity[i] <= comp) return 0;
-        }
-
-        long long ans = 1;
-        
-        for (int i = 1; i < complexity.size(); ++i){
+            
             ans = (ans * i) % MOD;
         }
 
-        return ans % MOD;
+        return ans;
     }
 };
