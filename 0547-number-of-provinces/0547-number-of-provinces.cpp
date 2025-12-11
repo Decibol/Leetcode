3877289588ccdd25e01d489e3c815107331f7be9
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2025-12-11 22:39:15 +0530

    Time: 3 ms (41.21%), Space: 19.5 MB (63.76%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,31 +1,64 @@
-class Solution {
+class DisjointSet {
+    vector<int> parent;
+    vector<int> size;
+
 public:
-    void dfs(int node, vector<vector<int>>& isConnected, vector<bool>& visited){
-        if (visited[node]) return;
+    DisjointSet(int n){
+        size.resize(n, 1);
+        parent.resize(n);
+        iota(parent.begin(), parent.end(), 0);
+    }
 
-        visited[node] = true;
+    int find(int node){
+        if (node == parent[node]){
+            return node;
+        }
 
-        for (int i = 0; i < isConnected.size(); ++i){
-            if (isConnected[node][i] == 1 && !visited[i]){
-                dfs(i, isConnected, visited);
-            }
+        return parent[node] = find(parent[node]);
+    }
+
+    void unionBySize(int u, int v){
+        int root_u = find(u);
+        int root_v = find(v);
+
+        if (root_u == root_v) return;
+
+        if (size[root_u] < size[root_v]){
+            parent[root_u] = root_v;
+            size[root_v] += size[root_u];
+        }
+        else {
+            parent[root_v] = root_u;
+            size[root_u] += size[root_v];
         }
     }
 
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int provinces = 0;
-        int n = isConnected.size();
-        if (n == 0) return 0;
+    int findParentCount(int n){
+        unordered_set<int> roots;
 
-        vector<bool> visited(n, false);
+        for (int i = 0; i < n; ++i){   
+            roots.insert(find(i));
+        }
+        
+        return roots.size();
+    }
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        DisjointSet ds(n);
+        int count = 0;
 
         for (int i = 0; i < n; ++i){
-            if (!visited[i]){
-                dfs(i, isConnected, visited);
-                ++provinces;
+            for (int j = 0; j < n; ++j){
+                if (isConnected[i][j] == 1){
+                    ds.unionBySize(i, j);
+                }
             }
         }
 
-        return provinces;    
+        return ds.findParentCount(n);
     }
 };
