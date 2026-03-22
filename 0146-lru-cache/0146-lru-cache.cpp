b1101b6f334538b663ed4da05e23860696315241
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2026-03-22 14:20:26 +0530

    Time: 62 ms (83.04%), Space: 173.1 MB (68.49%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,30 +1,30 @@
 class LRUCache {
-private:
+public:
     struct ListNode{
         int key, val;
-        ListNode *prev;
-        ListNode *next;
+        ListNode *prev, *next;
 
         ListNode(int k, int v) : key(k), val(v), prev(nullptr), next(nullptr) {}
     };
-
+    
     ListNode *head, *tail;
     unordered_map<int, ListNode*> cache;
     int capacity;
 
-    void removeNode(ListNode* node){
+    void insertAtHead(ListNode *node){
+        node->next = head->next;
+        head->next = node;
+        node->prev = head;
+        node->next->prev = node;
+    }
+
+    void removeNode(ListNode *node){
         node->prev->next = node->next;
         node->next->prev = node->prev;
     }
 
-    void insertAtHead(ListNode* node){
-        node->next = head->next;
-        node->prev = head;
-        head->next = node;
-        node->next->prev = node;
-    }
-public:
-    LRUCache(int capacity) : capacity(capacity) {
+    LRUCache(int capacity) {
+        this->capacity = capacity;
         head = new ListNode(0, 0);
         tail = new ListNode(0, 0);
 
@@ -34,32 +34,31 @@ public:
     
     int get(int key) {
         if (cache.find(key) == cache.end()) return -1;
+        
+        removeNode(cache[key]);
+        insertAtHead(cache[key]);
 
-        ListNode *node = cache[key];
-        removeNode(node);
-        insertAtHead(node);
-
-        return node->val;
+        return cache[key]->val;
     }
     
     void put(int key, int value) {
-        if (cache.find(key) != cache.end()){
+        if (cache.find(key) == cache.end()){
+            ListNode *newNode = new ListNode(key, value);
+            cache[key] = newNode;
+            insertAtHead(newNode);
+        }
+        else {
             ListNode *node = cache[key];
             node->val = value;
             removeNode(node);
             insertAtHead(node);
         }
-        else {
-            if (cache.size() == capacity){
-                ListNode *lru = tail->prev;
-                cache.erase(lru->key);
-                removeNode(lru);
-                delete lru;
-            }
 
-            ListNode *newNode = new ListNode(key, value);
-            cache[key] = newNode;
-            insertAtHead(newNode);
+        if (cache.size() > capacity){
+            ListNode *LRU = tail->prev;
+            cache.erase(LRU->key);
+            removeNode(LRU);
+            delete LRU;
         }
     }
 };
