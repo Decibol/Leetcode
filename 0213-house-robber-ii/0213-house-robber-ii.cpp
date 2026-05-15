d57d17a85ac6c88dc14d3ee9a737c2a695d1ff5f
Paras Vichoray <122614128+Decibol@users.noreply.github.com>
2026-05-15 20:43:59 +0530

    Time: 0 ms (100%), Space: 10.1 MB (96.9%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
+
+        int prev1 = nums[0];
+        int prev2 = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n - 1; ++i){
+            int temp = max(prev2, prev1 + nums[i]);
+            prev1 = prev2;
+            prev2 = temp;
+        }
+
+        int result = prev2;
+
+        prev1 = nums[1];
+        prev2 = max(nums[1], nums[2]);
+
+        for (int i = 3; i < n; ++i){
+            int temp = max(prev2, prev1 + nums[i]);
+            prev1 = prev2;
+            prev2 = temp;
+        }
+
+        return max(result, prev2);
+    }
+};
